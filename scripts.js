840adce91bae3bea45093e1945c2d91cafa2350a
lordcoudy <savva.x07@gmail.com>
2022-04-16 00:08:24 +0300

    Third and a half commit. Edited the editor's (lol) design a bit, added submit button (not well functioning yet)

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function submitFunction() {
+    window.location.replace("index.html");
+}
